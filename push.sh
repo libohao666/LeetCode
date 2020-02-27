135e50439c9c1f31bcfeaddacd3df6e44738763c
libohao666 <2052658718@qq.com>
2020-02-27 12:30:57 +0800

    20200227

--- push.sh
+++ push.sh
@@ -1,4 +1,17 @@
-#!/usr/bin/expect
+#!/bin/bash
+username = "libohao666"
+password = "libohao0"
+
+echo "=======提交开始======"
+
+git add *
+git commit -m $(date "+%Y%m%d")
+git push origin master
+
+expect<<EOF
 spawn ./push-1.sh
-expect "*Username*" {send "libohao666\r"}
-expect "*Password*" {send "libohao0\r"}
+expect "*Username*" {send "${username}\r"}
+expect "*Password*" {send "${password}\r"}
+EOF
+
+echo "======提交结束======"
