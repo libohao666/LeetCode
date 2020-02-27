2c46f2138b02503ac1018b29dda8e8c4363aaeb1
libohao666 <2052658718@qq.com>
2020-02-27 12:50:11 +0800

    20200227

--- push.sh
+++ push.sh
@@ -1,15 +1,4 @@
-#!/bin/bash
-username="libohao666"
-
-echo "\n=======提交开始======\n"
-
 git add *
 git commit -m $(date "+%Y%m%d")
-
-expect<<EOF
-spawn git push origin master
-set timeout 2
-expect "Username for" {send "${username}\r"}
-EOF
-echo "libohao0"
+git push origin master
 
