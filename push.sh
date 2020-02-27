dac1e0bae40cf32024676b873a86c6530134d933
libohao666 <2052658718@qq.com>
2020-02-27 12:22:37 +0800

    20200227

--- push.sh
+++ push.sh
@@ -3,4 +3,5 @@ spawn ./push-1.sh
 expect "*Username for 'https://github.com':*"
 send "libohao666\r"
 expect "*Password for 'https://libohao666@github.com':*"
-send "libohaoo0\r\r"
+send "libohaoo0\r"
+send "\n"
