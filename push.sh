a2f6e6f516d5fe7d60864fc30b9558db13940b23
libohao666 <2052658718@qq.com>
2020-02-27 12:33:02 +0800

    20200227

--- push.sh
+++ push.sh
@@ -9,8 +9,6 @@ git commit -m $(date "+%Y%m%d")
 git push origin master
 
 expect<<EOF
-set timeout 5
-spawn ./push-1.sh
 expect "Username for" {send "libohao666\r"}
 expect "Password for" {send "libohao0\r"}
 EOF
