033a64954d7d55c1f6f9629d26c5645f8979671f
libohao666 <2052658718@qq.com>
2020-02-27 12:48:49 +0800

    20200227

--- push.sh
+++ push.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 username="libohao666"
-password="libohao0"
 
 echo "\n=======提交开始======\n"
 
@@ -11,7 +10,5 @@ expect<<EOF
 spawn git push origin master
 set timeout 2
 expect "Username for" {send "${username}\r"}
-expect "Password for" {send "${libohao0}\r"}
 EOF
 
-echo "\n======提交结束======\n"
