c438e1dcea8fe7d73944b0c94d1086a95842e4b6
libohao666 <2052658718@qq.com>
2020-02-27 12:21:27 +0800

    20200227

--- push.sh
+++ push.sh
@@ -1,7 +1,5 @@
-git add *
-git commit -m $(date "+%Y%m%d")
-git push origin master
-#!/esr/bin/expect
+#!/usr/bin/expect
+spawn ./push-1.sh
 expect "*Username for 'https://github.com':*"
 send "libohao666\r"
 expect "*Password for 'https://libohao666@github.com':*"
